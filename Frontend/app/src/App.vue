aecafcad7dda5419d4026fa3f7c97dbf0abcd16e
Mickael Galmiche <just.angelion@gmail.com>
2022-01-01 18:06:23 +0100

    Add navigation component (header) and logo file

--- Frontend/app/src/App.vue
+++ Frontend/app/src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header>
+    <NavMenu/>
+  </header>
+  <main>
+    <router-view/>
+  </main>
 </template>
+
+<script>
+import NavMenu from '@/components/NavMenu.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NavMenu
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -13,18 +25,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ Frontend/app/src/components/NavMenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav id="navMenu">
+        <router-link :to="{ name: 'Home' }"><img src="@/assets/logo/icon-left-font.svg" alt="Groupomania Logo"></router-link>
+        <ul>
+            <li><router-link :to="{ name: 'About' }">About</router-link></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'navMenu'
+}
+</script>
+
+<style lang="scss">
+    #navMenu {
+        padding: 0 30px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        img {
+            width: 250px;
+            height: 100px;
+            object-fit: cover;
+        }
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+              color: #42b983;
+            }
+        }
+
+        ul {
+            list-style: none;
+        }
+    }
+</style>
